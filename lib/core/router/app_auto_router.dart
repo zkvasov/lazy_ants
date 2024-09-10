6631af3c4686dbe87e60bb6c1d711e53ce5d687d
Yevhen Kvasov <zkvasov07@gmail.com>
2024-09-10 19:08:22 +0300

    screen transaction animations

--- lib/core/router/app_auto_router.dart
+++ lib/core/router/app_auto_router.dart
@@ -1,32 +1,40 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
 
 import 'app_auto_router.gr.dart';
 
-// class _AppRoute extends AutoRoute {
-//  _AppRoute (
-//   {
-//     required super.page,
-//     required super.path,
+const _durationInMilliseconds = 300;
 
-//   }
-// ) : super();
-// }
+class _AppRoute extends CustomRoute {
+  _AppRoute({
+    required super.page,
+    required super.path,
+    RouteTransitionsBuilder transitionsBuilder = TransitionsBuilders.fadeIn,
+    super.children,
+  }) : super(
+          transitionsBuilder: transitionsBuilder,
+          durationInMilliseconds: _durationInMilliseconds,
+          reverseDurationInMilliseconds: _durationInMilliseconds,
+        );
+}
 
 @AutoRouterConfig()
 class AppAutoRouter extends RootStackRouter {
   @override
   List<AutoRoute> get routes => [
-        AutoRoute(
+        _AppRoute(
           page: LoginRoute.page,
           path: '/',
         ),
-        AutoRoute(
+        _AppRoute(
           page: UsersRoute.page,
           path: '/users',
+          transitionsBuilder: TransitionsBuilders.slideRight,
         ),
-        AutoRoute(
+        _AppRoute(
           page: UserDetailsRoute.page,
           path: '/user-details',
+          transitionsBuilder: TransitionsBuilders.slideLeft,
         ),
       ];
 }
